bc7fcb186877e985cef145d14cf866db220e4fcb
James Hui <huitmj@gmail.com>
2025-01-05 23:08:06 +0000

    Create build_uboot.sh

---
+++ build_uboot.sh
@@ -0,0 +1,3 @@
+wget -qO- https://github.com/u-boot/u-boot/archive/refs/tags/v2024.10.tar.gz | tar zxv
+wget -qO- https://toolchains.bootlin.com/downloads/releases/toolchains/armv7m/tarballs/armv7m--uclibc--stable-2024.05-1.tar.xz | tar xv
+
