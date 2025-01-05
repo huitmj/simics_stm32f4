fffc69ae9c857f8fb63326553e50864a31a5bf93
James Hui <huitmj@gmail.com>
2025-01-05 23:46:44 +0000

    add CC and uboot

--- build_uboot.sh
+++ build_uboot.sh
@@ -1,3 +1,16 @@
-wget -qO- https://github.com/u-boot/u-boot/archive/refs/tags/v2024.10.tar.gz | tar zxv
-wget -qO- https://toolchains.bootlin.com/downloads/releases/toolchains/armv7m/tarballs/armv7m--uclibc--stable-2024.05-1.tar.xz | tar xv
+wget -qO- https://github.com/u-boot/u-boot/archive/refs/tags/v2024.10.tar.gz | tar zx
+wget -qO- https://toolchains.bootlin.com/downloads/releases/toolchains/armv7m/tarballs/armv7m--uclibc--stable-2024.05-1.tar.xz | tar Jx
+wget -qO- https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/armv7-eabihf--glibc--stable-2024.05-1.tar.xz | tar Jx
+wget -qO- https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/armv7-eabihf--musl--stable-2024.05-1.tar.xz  | tar Jx
 
+
+sudo apt update
+sudo apt install -y flex
+
+export CROSS_COMPILE=${PWD}/armv7m--uclibc--stable-2024.05-1/bin/arm-linux-
+export CROSS_COMPILE=${PWD}/armv7-eabihf--glibc--stable-2024.05-1/bin/arm-linux-
+export CROSS_COMPILE=${PWD}/armv7-eabihf--musl--stable-2024.05-1/bin/arm-linux-
+
+cd u-boot-2024.10
+make stm32f429-discovery_defconfig
+make -j 2
